bc5ed1ef0b0accd0fc97aeb7615132d8af451e32
md-amir-gauhar <gauharamir15@gmail.com>
2022-02-01 23:54:44 +0530

    fix: UI issue

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -5,6 +5,9 @@ const Animal = ({ currentAnimals, loading }) => {
   return (
     <div className="list-container">
       {
+        loading && <h3 className="loading">Loading.....</h3>
+      }
+      {
         currentAnimals.map(animal => (
           <Card key={animal.id} animal={animal} />
         ))
